3a02798c14d5ca2102de259909e725fce139e2a0
Carlos <carlossolarte340@gmail.com>
2026-08-10 15:07:03 -0400

    'v8.0.4'

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------------
 // Constants
 // -----------------------------------------------------------------------------
-export const APP_VERSION = 'v8.0.3'
+export const APP_VERSION = 'v8.0.4'
 export const IS_PRODUCTION = true // Change to false for localhost testing;
