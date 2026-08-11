e9a0a2fe118c3878862fe990e5ab3ab61909e840
Carlos <carlossolarte340@gmail.com>
2026-08-11 11:32:42 -0400

    feat: bump to v8.2.0

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------------
 // Constants
 // -----------------------------------------------------------------------------
-export const APP_VERSION = 'v8.1.0'
+export const APP_VERSION = 'v8.2.0'
 export const IS_PRODUCTION = true // Change to false for localhost testing;
