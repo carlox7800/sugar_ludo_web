79d5dc0f903819a052b7b60d33d111f6a7ecc1e3
Carlos <carlossolarte340@gmail.com>
2026-08-12 10:08:56 -0400

    feat: bump version to v8.2.7 with server 15s turn timer and bonus deduction

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------------
 // Constants
 // -----------------------------------------------------------------------------
-export const APP_VERSION = 'v8.2.6'
+export const APP_VERSION = 'v8.2.7'
 export const IS_PRODUCTION = true // Change to false for localhost testing;
