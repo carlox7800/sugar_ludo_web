d6f6775d8e5cc9a74f24ef3dd31716113709330a
Carlos <carlossolarte340@gmail.com>
2026-08-14 14:33:12 -0400

    Fix Render deploy: Use server.js with dynamic PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const url = require('url');
 
 const OUT_DIR = path.join(__dirname, 'out');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const MIME_TYPES = {
   '.html': 'text/html; charset=utf-8',
