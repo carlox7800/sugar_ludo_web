c23024f915bfb6a525c126510c9ce3c6ebc77893
Carlos <carlossolarte340@gmail.com>
2026-08-26 14:29:40 -0400

    fix(engine): Sugar Ludo v8.6.3 - Restauracion de importacion useCallback en OnlineGameEngine para acceso fluido al tablero

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------------
 // Constants
 // -----------------------------------------------------------------------------
-export const APP_VERSION = 'v8.6.2'
+export const APP_VERSION = 'v8.6.3'
 export const IS_PRODUCTION = true // Change to false for localhost testing;
