ba272dab4819850a2f4043a1c1efc6b5b86901e7
Carlos <carlossolarte340@gmail.com>
2026-09-04 13:40:21 -0400

    correccion(version): unificar APP_VERSION en juego web con lib/version.ts v8.8.6

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,7 @@
 // -----------------------------------------------------------------------------
 // Constants
 // -----------------------------------------------------------------------------
-export const APP_VERSION = 'v8.8.5'
+import { APP_VERSION_TAG } from './version'
+
+export const APP_VERSION = APP_VERSION_TAG
 export const IS_PRODUCTION = true // Change to false for localhost testing;
