7700fd41b6c270304c30b8a094d9dde5981da096
Carlos <carlossolarte340@gmail.com>
2026-09-18 18:21:55 -0400

    chore(release): bump a v9.4.3 y despliegue modularizacion de cajero TEC-03

--- sugar-ludo-landing/components/GamerHUDModal.tsx
+++ sugar-ludo-landing/components/GamerHUDModal.tsx
@@ -15,7 +15,7 @@ export const GamerHUDModal: React.FC<GamerHUDModalProps> = ({
   isOpen,
   onClose,
   platform,
-  version = '9.4.2',
+  version = '9.4.3',
   downloadUrl,
 }) => {
   useEffect(() => {
