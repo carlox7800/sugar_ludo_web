d6a613dda5a4f179d587179a99eda25adaaf152d
Carlos <carlossolarte340@gmail.com>
2026-09-18 15:30:20 -0400

    chore(release): sincronizar firestore.rules con produccion y bump a v9.3.8

--- sugar-ludo-landing/components/GamerHUDModal.tsx
+++ sugar-ludo-landing/components/GamerHUDModal.tsx
@@ -15,7 +15,7 @@ export const GamerHUDModal: React.FC<GamerHUDModalProps> = ({
   isOpen,
   onClose,
   platform,
-  version = '9.3.7',
+  version = '9.3.8',
   downloadUrl,
 }) => {
   useEffect(() => {
