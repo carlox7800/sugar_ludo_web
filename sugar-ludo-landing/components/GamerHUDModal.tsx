8db2cfbadf0cac5cb6a695ee92cdf9a936aabf25
Carlos <carlossolarte340@gmail.com>
2026-09-18 16:53:38 -0400

    chore(release): bump a v9.4.1 y despliegue de tipado estricto TEC-02

--- sugar-ludo-landing/components/GamerHUDModal.tsx
+++ sugar-ludo-landing/components/GamerHUDModal.tsx
@@ -15,7 +15,7 @@ export const GamerHUDModal: React.FC<GamerHUDModalProps> = ({
   isOpen,
   onClose,
   platform,
-  version = '9.4.0',
+  version = '9.4.1',
   downloadUrl,
 }) => {
   useEffect(() => {
